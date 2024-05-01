a31ff1ab5136f2e8b6011e49fb3b8a9821c079ea
elio-info <dario.andreu@gmail.com>
2024-04-30 20:06:20 -0400

    haciendo algo

--- utls/js/funcs.js
+++ utls/js/funcs.js
@@ -7,7 +7,8 @@ let ciencia_invest_art=[
     "autor": ['Enrique Orestes Santos. Investigador independiente.',
               'Reinaldo Hanoi Valdés Reinoso. Doctor en Ciencias Forestales.',
               'Bertha Rita Castillo EduaDoctora en Ciencias Forestales.'],
-    "ruta":"/sc-inv/articulo-publicado-rap05124.pdf"                    
+    "ruta":"utls/sc-inv/articulo-publicado-rap05124.pdf",
+    "ico":"utls/sc-inv/paper-64.ico"
 }
 ]
 
@@ -59,33 +60,91 @@ function cambioCiencia(params) {
   }
 }
 
+/**
+ * @param {string} tipo_doc donde se pone
+ * @param {string} literatureFolder arreglos de datos 
+ * @param {Array} literatureData arreglos de datos 
+ * @param {icon} iconDatAutor icono que lleva
+ * @void 
+ */
+function fillLiterature(literatureData) {
+  //crear estructura cuerpo tarjeta Documento
+  // imagen-tipo-literatura {enlace-doc > imagen-tipo-doc-extension año-doc titulo-doc}
+  //                           [autor-doc > imagen-tipo-autor nombre-autor]
+
+  //dentro de la tarjeta completa, el nombre
+  let literatureCard_p = document.createElement('p');
+
+  //dentro de la tarjeta completa, agrego el icono del tipo de Doc
+  let doc_tipo_ico = document.createElement("img");
+  doc_tipo_ico.src = literatureData.ico;//poner icono lugar valor
+  //doc_tipo_ico.className = tipo_doc.clase;//poner icono clase valor
+
+  literatureCard_p.appendChild(doc_tipo_ico);//agrego dentro de la tarjeta completa, agrego el icono del tipo de Doc
+  
+ 
+  //dentro de la tarjeta completa, el icono
+  let doc_pdfLink = document.createElement("a");  
+  doc_pdfLink.href =literatureData.ruta;  
+
+  let titulo=document.createElement('span')
+  titulo.innerHTML='&nbsp; '+literatureData.nomb
+  doc_pdfLink.appendChild(titulo);//agrego espacio entre img y vinculo
+
+  literatureCard_p.appendChild(doc_pdfLink);//agrego dentro de la tarjeta completa, enlace de Doc 
+ 
+//crear el div
+  let div_literatura = document.createElement("div");
+  div_literatura.appendChild(literatureCard_p);//agrego dentro de la tarjeta completa, agrego el p
+
+  return div_literatura;
+
+}
+
+
+function crearAccStack(param) {
+  let div_row=document.createElement('div')//creando eleto row
+    div_row.setAttribute( 'class','row')
+    let algo=fillLiterature(param)
+    div_row.appendChild(algo)
+    let div_row_inside=document.createElement('div')//creando eleto row de bg-success text-white
+    //div_row_inside.setAttribute( 'class','bg-success text-white')
+    let div_row_inside_h4=document.createElement('h4')//creando eleto row de bg-success text-white
+        div_row_inside_h4.className='bg-success text-white'
+        div_row_inside_h4.innerHTML=param.nomb
+    let div_row_inside_ref=document.createElement('a')//creando eleto row de bg-success text-white
+        div_row_inside_ref.href=param.ruta
+        //imagen
+          let div_row_inside_ref_img=document.createElement('img')//creando eleto row de bg-success text-white
+          div_row_inside_ref_img.src=param.ico
+          div_row_inside_ref_img.alt=param.nomb
+          div_row_inside_ref.appendChild(div_row_inside_ref_img) 
+          let art_ref=document.createElement('p')//creando eleto row de bg-success text-white
+          art_ref.innerHTML= param.nomb.substring(0,10)+'...'
+          div_row_inside_ref.appendChild(art_ref)
+
+      div_row_inside.appendChild(div_row_inside_h4)//incorporar h4
+      div_row_inside.appendChild(div_row_inside_ref)//incorporar vinculo
+
+    div_row.appendChild(div_row_inside)//incorporar row_inside
+  return div_row
+}
+
 function llenarAcc(data,root) {
-  // let prod_fotos=data.fotos,
-  //     ponerCarousel=document.getElementById('thumbsSeccion')
+  let prod_fotos=data.fotos,
+      ponerDatos=document.getElementById('producto-oferta')
   // llenar las imagenes
   // 1-vaciar
-  // while (ponerCarousel.firstChild) {
-  //     // The list is LIVE so it will re-index each call
-  //     ponerCarousel.removeChild(ponerCarousel.firstChild);
-  //   }
+  while (ponerDatos.firstChild) {
+      // The list is LIVE so it will re-index each call
+      ponerDatos.removeChild(ponerDatos.firstChild);
+    }
   // llenar el trip
-  data.map(e =>{
-      let imgVinc=document.createElement('img'),
-          atta="'"+e+"','"+root+"'"
-
-          imgVinc.setAttribute( 'src','utls/imgs/'+root+'/IMG-'+e+'.jpg')
-          imgVinc.setAttribute('id','e')
-          imgVinc.setAttribute('alt','ads')
-          imgVinc.setAttribute('onclick','mostrarEnCarrusel('+atta+');')
-          imgVinc.setAttribute('className',"imagenScala20")
-     
-      ponerCarousel.appendChild(imgVinc) 
-  })
-  // fotos
-  // cambiar Foto Principal
-  mostrarEnCarrusel(prod_fotos[0],root);
+  data.map(e =>{    
+      ponerDatos.appendChild(crearAccStack(e)) 
+  }) 
   // descripcion de ofrecemos
-  document.getElementById('ofrece').innerHTML=data.ofrece
+ // document.getElementById('ofrece').innerHTML=data.ofrece
 }
 
 
